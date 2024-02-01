ee71cb8ae7740d88f58f5c6f27eeb418373df9ed
peperoli <shagix.ix@gmail.com>
2024-02-01 12:07:13 +0100

    Enhancement: Add text field to event_item, remove dayjs dep

--- components/event_item.js
+++ components/event_item.js
@@ -2,17 +2,17 @@ import React from 'react'
 import { storyblokEditable, StoryblokComponent } from '@storyblok/react'
 import Image from 'next/image'
 import Icon from './helpers/icon.js'
-import dayjs from 'dayjs'
-import 'dayjs/locale/de'
 
 const EventItem = ({ blok }) => {
+  const dateStart = new Date(blok.date_start)
+  const dateEnd = new Date(blok.date_end)
   return (
     <li className="event col-span-full md:col-span-6 xl:col-span-4 p-6 rounded-lg bg-neutral-100" {...storyblokEditable(blok)}>
       <div className="flex gap-3 pb-4">
         <div className="flex flex-col justify-center min-w-20 p-4 text-center text-white font-bold rounded bg-red-600">
-          <span>{dayjs(blok.date_start).format('dd')}</span>
-          <span className="block text-4xl">{dayjs(blok.date_start).format('D.')}</span>
-          <span>{dayjs(blok.date_start).locale('de-ch').format('MMM')}</span>
+          <span>{dateStart.toLocaleDateString('de-CH', { weekday: 'short' })}</span>
+          <span className="block text-4xl">{dateStart.toLocaleDateString('de-CH', { day: 'numeric' })}</span>
+          <span>{dateStart.toLocaleDateString('de-CH', { month: 'short' })}</span>
         </div>
         {blok.picture.filename &&
           <div className="w-full aspect-w-2 aspect-h-1">
@@ -29,11 +29,12 @@ const EventItem = ({ blok }) => {
       </div>
       <div>
         {blok.headline && <h4>{blok.headline}</h4>}
+        {blok.text && <p className="richtext mb-4 whitespace-pre-line">{blok.text}</p>}
         {blok.date_start &&
           <div className="flex gap-2">
             <Icon name="time-fill" className="text-red-600" />
-            <div>{dayjs(blok.date_start).format('H.mm')}
-              {blok.date_end && <span> – {dayjs(blok.date_end).format('H.mm')} Uhr</span>}</div>
+            <div>{dateStart.toLocaleTimeString('de-CH', { timeStyle: 'short' })}
+              {dateEnd && <span>–{dateEnd.toLocaleTimeString('de-CH', { timeStyle: 'short' })} Uhr</span>}</div>
           </div>}
         {blok.location &&
           <div className={`flex gap-2 mt-1${blok.button?.length ? ' mb-4' : ''}`}>
